bd52703ed455961f882d5a76660d2a7eb5d4d6c8
Ayushmangela <ayushmangela@gmail.com>
2025-09-11 20:01:49 +0530

    small change

---
+++ lib/config/api_keys.dart
@@ -0,0 +1,4 @@
+class ApiKeys {
+  static const String supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpqYW1qcGxxeGZrbmN5a3JidHlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc0MDE2ODksImV4cCI6MjA3Mjk3NzY4OX0.4YH9okB35ruFzTGt9FXDml5Qq1AkLkwp4xFb4a9u2ao';
+}
--- lib/config/supabase_config.dart
+++ lib/config/supabase_config.dart
@@ -1,5 +1,6 @@
+import 'api_keys.dart';
+
 class SupabaseConfig {
   static const String supabaseUrl = 'https://zjamjplqxfkncykrbtyp.supabase.co';
-  static const String supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpqYW1qcGxxeGZrbmN5a3JidHlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc0MDE2ODksImV4cCI6MjA3Mjk3NzY4OX0.4YH9okB35ruFzTGt9FXDml5Qq1AkLkwp4xFb4a9u2ao';
+  static const String supabaseAnonKey = ApiKeys.supabaseAnonKey;
 }
